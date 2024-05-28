9f3541f2dee336ae241257a6b80bc37edea0d7b9
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-05-28 21:58:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18252 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.print(":fan::fan::fan:\n" +
+                ":fan::"+str+"::fan:\n" +
+                ":fan::fan::fan:");
+    }
+}
