26d8a004d5e47cdf32ddb7208ab0f6f2343dbbf9
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-05-30 10:36:55 +0900

    [Bronze V] Title: Site Score, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        int d = s.nextInt();
+
+        System.out.print(56*a+24*b+14*c+6*d);
+    }
+}
