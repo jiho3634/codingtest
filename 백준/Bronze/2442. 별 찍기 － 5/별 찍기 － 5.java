b4548c99faacb48db8b7d94fd70ebc1c5c867300
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-07 11:58:44 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(" ".repeat(n - i - 1) + "*".repeat(2 * i + 1));
+        }
+    }
+}
