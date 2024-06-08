67131818374e370b3ce16a72f04428f7759273e0
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-08 16:05:11 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 212 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println((int)Math.pow(Math.pow(2,s.nextInt()) + 1, 2));
+    }
+}
