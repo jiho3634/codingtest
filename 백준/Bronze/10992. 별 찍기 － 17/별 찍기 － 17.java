5a464fd9917390c821558e1ee59ae9ea92c0b3d8
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-08 17:27:34 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 208 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -1,33 +1,12 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int N = s.nextInt();
-        s.close();
-
-        for (int i = 0; i < N; i++) {
-            // 출력할 줄의 공백
-            for (int j = 0; j < N - i - 1; j++) {
-                System.out.print(" ");
-            }
-            
-            // 첫 번째 줄부터 N-1번째 줄까지는 공백과 별 패턴
-            if (i != N - 1) {
-                System.out.print("*");
-                for (int j = 0; j < 2 * i - 1; j++) {
-                    System.out.print(" ");
-                }
-                if (i > 0) {
-                    System.out.print("*");
-                }
-            } else {
-                // 마지막 줄은 모두 별
-                for (int j = 0; j < 2 * N - 1; j++) {
-                    System.out.print("*");
-                }
-            }
-            System.out.println();
-        }
+        int n = s.nextInt();
+        System.out.println(" ".repeat(n - 1) + "*");
+        for (int i = 2; i < n; i++)
+            System.out.println(" ".repeat(n - i) + "*" + " ".repeat(2 * i - 3) + "*");
+        if (n != 1)
+            System.out.print("*".repeat(2 * n - 1));
     }
 }
