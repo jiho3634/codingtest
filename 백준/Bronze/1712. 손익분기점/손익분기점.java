6c58db23ba6ea4a4ed42ab8968d5bbc866cd044e
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-11 21:25:35 +0900

    [Bronze II] Title: 손익분기점, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.java
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -2,17 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int fc = s.nextInt();
-        int vc = s.nextInt();
-        int p = s.nextInt();
-        if (vc == p) {
-            System.out.println(-1);
-            return ;
-        }
-        int a = -fc / (vc - p);
-        if (a < 0)
+        int fc = s.nextInt(), vc, p, a;
+        if ((vc = s.nextInt()) == (p = s.nextInt()))
             System.out.println(-1);
         else
-            System.out.print(a + 1);
+            System.out.println((a = -fc / (vc - p)) < 0 ? -1 : (a + 1));
     }
 }
