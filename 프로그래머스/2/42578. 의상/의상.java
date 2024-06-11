86754ed3decbb86a3fd8834da34d33924fa9978d
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-11 11:41:24 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++)
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        Iterator<String> it = map.keySet().iterator();
+        while (it.hasNext()) 
+            answer = answer * (map.get(it.next()) + 1);
+        return answer - 1;
+    }
+}
