a659149210f7869c2c7aa033c10d55155d24126b
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-13 17:12:23 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17740 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,10 +6,13 @@ public class Main {
         s.nextLine();
         String m = s.nextLine();
         long sum = 0;
+        long mod = 1234567891;
+        long p = 1;
+
         for (int i = 0; i < l; i++) {
-            sum += (m.charAt(i) - 'a' + 1) * (long)Math.pow(31, i);
+            sum = (sum + (m.charAt(i) - 'a' + 1) * p) % mod;
+            p = (p * 31) % mod;
         }
-        long H = sum % 1234567891;
-        System.out.println(H);
+        System.out.println(sum);
     }
 }
