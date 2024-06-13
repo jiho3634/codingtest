4ae20f54bd1f005651cd29a7181100bb68c4fdff
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-13 21:13:05 +0900

    [Bronze II] Title: 8진수 2진수, Time: 2368 ms, Memory: 90376 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -1,21 +1,8 @@
 import java.util.Scanner;
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String a = s.nextLine();
-        for (int i = 0; i < a.length(); i++) {
-            int b = a.charAt(i) - '0';
-            String tmp = "" + b / 4 + b % 4 / 2 + b % 4 % 2;
-            if (i == 0) {
-                if (b / 4 == 0) {
-                    if (b % 4 / 2 == 0) {
-                        tmp = "" + b % 4 % 2;
-                    } else {
-                        tmp = "" + b % 4 / 2 + b % 4 % 2;
-                    }
-                }
-            }
-            System.out.print(tmp);
-        }
+        System.out.print(new Scanner(System.in).nextBigInteger(8).toString(2));
     }
 }
