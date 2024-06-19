1aa11eb9edee7be8b0bdee69063b3f6557ac8c9f
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-19 11:51:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2604 ms, Memory: 346624 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -4,34 +4,21 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 입력을 빠르게 받기 위해 BufferedReader 사용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        // 첫 줄에서 수의 개수 N 읽기
-        int N = Integer.parseInt(br.readLine());
-        
-        // N개의 수를 저장할 배열 생성
-        int[] numbers = new int[N];
-        
-        // N개의 수를 배열에 입력
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        
-        // 배열을 오름차순으로 정렬
-        Arrays.sort(numbers);
-        
-        // 정렬된 배열을 출력
-        for (int number : numbers) {
-            bw.write(String.valueOf(number));
-            bw.newLine();
+        Arrays.sort(arr);
+        for (Integer i : arr) {
+            bw.write((i + "\n"));
         }
-        
-        // BufferedWriter를 닫아 출력 완료
         bw.flush();
         bw.close();
         br.close();
