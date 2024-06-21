d9d60d7e9a6998727662de419f61d82d75678f8d
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-21 17:24:52 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.47 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+class Solution {
+    public int[] solution(int k, int[] score) {
+                Queue<Integer> pq = new PriorityQueue<>();
+        int[] answer = new int[score.length];
+        for (int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            if (i >= k) {
+                pq.poll();
+            }
+            answer[i] = pq.peek();
+        }
+        return answer;
+    }
+}
