063f1229c5716d9408d63767d25f16b84f7fda6c
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-24 14:16:47 +0900

    [Bronze I] Title: 나무 조각, Time: 148 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] numbers = br.readLine().strip().split(" ");
+        int[] arr = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = Integer.parseInt(numbers[i]);
+        }
+        for (int i = arr.length; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    for (int k : arr) {
+                        bw.write(k + " ");
+                    }
+                    bw.newLine();
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
