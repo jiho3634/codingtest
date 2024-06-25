bf9ab5bc8027516370ed656cb7d01ba776f6a194
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-25 21:28:08 +0900

    [Bronze I] Title: 유진수, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/1356. 유진수/유진수.java
+++ 백준/Bronze/1356. 유진수/유진수.java
@@ -1,37 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String number = scanner.next();
-        scanner.close();
-        
-        boolean isEugeneNumber = false;
-        
-        // 문자열을 1자리부터 n-1자리까지 두 부분으로 나눔
-        for (int i = 1; i < number.length(); i++) {
-            String leftPart = number.substring(0, i);
-            String rightPart = number.substring(i);
-            
-            if (getProduct(leftPart) == getProduct(rightPart)) {
-                isEugeneNumber = true;
-                break;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String n = br.readLine();
+        for (int i = 1; i < n.length(); i++) {
+            long a = 1;
+            for (int j = 0; j < i; j++) {
+                a *= n.charAt(j) - '0';
+            }
+            long b = 1;
+            for (int j = i; j < n.length(); j++) {
+                b *= n.charAt(j) - '0';
+            }
+            if (a == b) {
+                bw.write("YES");
+                bw.flush();
+                bw.close();
+                br.close();
+                return ;
             }
         }
-        
-        if (isEugeneNumber) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
-    }
-    
-    // 문자열로 주어진 숫자의 각 자릿수의 곱을 계산하는 메소드
-    private static long getProduct(String part) {
-        long product = 1;
-        for (char c : part.toCharArray()) {
-            product *= c - '0'; // '0'의 아스키 값을 빼서 실제 숫자로 변환
-        }
-        return product;
-    }
+        bw.write("NO");
+        bw.flush();
+        bw.close();
+        br.close();
+   }
 }
+
