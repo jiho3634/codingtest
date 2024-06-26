36dc270ceb4a7170566c734a2cc701a2389f3d18
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-26 20:51:37 +0900

    [Silver V] Title: 단어 정렬, Time: 568 ms, Memory: 21864 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,27 +1,24 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        String[] lines = new String[n];
-        for (int i = 0; i < n; i++) {
-            lines[i] = br.readLine();
-        }
-        Arrays.sort(lines, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return Integer.compare(o1.length(), o2.length());
+        Set<String> set = new TreeSet<>((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
             }
+            return Integer.compare(o1.length(), o2.length());
         });
-        List<String> list = Arrays.stream(lines).distinct().collect(Collectors.toList());
-        for (String s : list) {
+
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        for (String s : set) {
             System.out.println(s);
         }
         bw.flush();
