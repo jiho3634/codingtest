b1969e61a9d40b0a3ef36b1b5d1c01944f0bce78
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-28 17:26:30 +0900

    [Gold V] Title: 애너그램, Time: 1084 ms, Memory: 30968 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -2,41 +2,38 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     static char[] buffer;
+    static boolean[] used;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.nextLine();
         for (int i = 0; i < n; i++) {
             char[] s = scanner.nextLine().toCharArray();
-            boolean[] used = new boolean[s.length];
+            used = new boolean[s.length];
             buffer = new char[s.length];
             Arrays.sort(s);
-            dfs(s, used, 0);
+            dfs(s, 0);
         }
     }
 
-    static void dfs(char[] s, boolean[] used, int len) {
+    static void dfs(char[] s, int len) {
         if (len == s.length) {
             System.out.println(buffer);
         }
-
         else {
-            boolean[] arr = new boolean[s.length];
+            boolean[] used2 = new boolean['z' - 'a' + 1];
             for (int i = 0; i < s.length; i++) {
-                if (!used[i] && !arr[i]) {
+                if (!used[i]) {
                     buffer[len] = s[i];
                     used[i] = true;
-                    dfs(s, used.clone(), len + 1);
+                    dfs(s, len + 1);
                     used[i] = false;
-                    for (int j = 0; j < s.length; j++) {
-                        if (s[j] == s[i] && !used[j]) {
-                            arr[j] = true;
-                        }
-                    }
+                    while (i + 1 < s.length && s[i] == s[i + 1]) i++;
                 }
             }
         }
     }
 }
-
