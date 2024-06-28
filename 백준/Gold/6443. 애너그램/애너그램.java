28eab52f4883c8516aa3ac729d10bb2d7bb5137c
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-06-28 17:53:21 +0900

    [Gold V] Title: 애너그램, Time: 308 ms, Memory: 32836 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,30 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
+    static StringBuilder sb = new StringBuilder();
     static char[] buffer;
     static boolean[] used;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
-            char[] s = scanner.nextLine().toCharArray();
-            used = new boolean[s.length];
+            char[] s = br.readLine().toCharArray();
             buffer = new char[s.length];
+            used = new boolean[s.length];
             Arrays.sort(s);
             dfs(s, 0);
         }
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
     static void dfs(char[] s, int len) {
-        if (len == s.length) {
-            System.out.println(buffer);
-        }
+        if (len == s.length) sb.append(buffer).append("\n");
         else {
-            boolean[] used2 = new boolean['z' - 'a' + 1];
             for (int i = 0; i < s.length; i++) {
                 if (!used[i]) {
                     buffer[len] = s[i];
