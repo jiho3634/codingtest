e636853d138d7609f41f386b5405f3951516884b
Jiho Kim <117874745+jiho3634@users.noreply.github.com>
2024-10-20 14:11:30 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,15 +1,23 @@
-import java.util.Map;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Map;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++)
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
-        Iterator<String> it = map.keySet().iterator();
-        while (it.hasNext()) 
-            answer = answer * (map.get(it.next()) + 1);
-        return answer - 1;
+        Map<String, Integer> clothingMap = new HashMap<>();
+
+        // 각 의상 종류별로 의상의 개수를 카운팅
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            clothingMap.put(type, clothingMap.getOrDefault(type, 0) + 1);
+        }
+
+        int combinations = 1;
+        // 각 종류별 (의상 개수 + 1)을 곱함 (해당 종류의 의상을 입지 않는 경우를 고려)
+        for (int count : clothingMap.values()) {
+            combinations *= (count + 1);
+        }
+
+        // 최소 한 개의 의상은 입어야 하므로 아무것도 입지 않은 경우를 제외
+        return combinations - 1;
     }
 }
